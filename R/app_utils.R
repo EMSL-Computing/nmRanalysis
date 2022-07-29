fc060b31e3e45336534b78d4654c109a16e97a55
Flores, Javier E <javier.flores@pnnl.gov>
2022-07-29 10:19:33 -0700

    Added a function to determine the mode of a vector. This is useful if/when aggregating duplicate reference entries (by duplicate, I mean under teh same set of experimental conditions). Since the multiplicity is character-valued, when aggregating we need to find the mode as opposed to a mean.

--- R/app_utils.R
+++ R/app_utils.R
@@ -28,6 +28,36 @@
   !(a %in% b)
 }
 
+#' Return the mode of a vector
+#'
+#' @description Copyright (C) 2022 Battelle Memorial Institute
+#'
+#'  This program is free software; you can redistribute it and/or modify
+#'  it under the terms of the GNU General Public License as published by
+#'  the Free Software Foundation; either version 2 of the License, or
+#'  (at your option) any later version.
+#'
+#'  This program is distributed in the hope that it will be useful,
+#'  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#'  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#'  GNU General Public License for more details.
+#'
+#'  You should have received a copy of the GNU General Public License along
+#'  with this program; if not, write to the Free Software Foundation, Inc.,
+#'  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#'
+#' @param x A vector
+#'
+#' @details This function determines the most common value in a vector. In the event of a tie, this function returns
+#' the value that is first as ordered by \code{table()}. Whether the vector is numeric or character, this
+#' function returns a character-valued scalar.
+#'
+#' @return A character-valued scalar.
+#'
+getmode <- function(x, ...){
+  names(which.max(table(x, ...)))
+}
+
 
 #' Load experimental NMR data, experimental NMR metadata, or target metabolite data
 #'
