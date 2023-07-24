0f7782bc253eadb39f64320a308e425626547716
Lewis, Logan A <logan.lewis@pnnl.gov>
2023-07-24 14:22:13 -0700

    removing trelliscope functionality as a test

--- R/connect_db.R
+++ R/connect_db.R
@@ -26,18 +26,18 @@
 connect_db <- function(){
     # Set environmental variables
 
-    # dsn_database = "nmRanalysis"
-    # dsn_port = "5432"
-    # dsn_hostname = "localhost"
-    # dsn_uid = "postgres"
+    dsn_database = "nmRanalysis"
+    dsn_port = "5432"
+    dsn_hostname = "localhost"
+    dsn_uid = "postgres"
     # dsn_uid = "developer"
-    # dsn_pwd = "developer"
+    dsn_pwd = "developer"
 
-    dsn_database = Sys.getenv(c("DATABASE_NAME"))
-    dsn_hostname = Sys.getenv(c("DATABASE_HOST"))
-    dsn_port = Sys.getenv(c("DATABASE_PORT"))
-    dsn_uid = Sys.getenv(c("DATABASE_USER"))
-    dsn_pwd = Sys.getenv(c("DATABASE_PASSWORD"))
+    # dsn_database = Sys.getenv(c("DATABASE_NAME"))
+    # dsn_hostname = Sys.getenv(c("DATABASE_HOST"))
+    # dsn_port = Sys.getenv(c("DATABASE_PORT"))
+    # dsn_uid = Sys.getenv(c("DATABASE_USER"))
+    # dsn_pwd = Sys.getenv(c("DATABASE_PASSWORD"))
 
     # Connect to Database
     tryCatch({
